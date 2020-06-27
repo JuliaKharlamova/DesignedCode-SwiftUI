05553c19f1c8b8485e88b8a94385e7f8bcfb582d
Julia <kharlamovajulia@icloud.com>
2020-06-27 15:17:36 +1000

    Initial Commit

---
+++ DesignedCode SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignedCode SwiftUI
+//
+//  Created by Юлия Харламова on 27.06.2020.
+//  Copyright © 2020 Юлия Харламова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
